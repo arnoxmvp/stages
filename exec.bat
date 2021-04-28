26ddb5bc9ad6ccb85560c03f94951c342b9bdbb5
arnoxmvp <82814715+arnoxmvp@users.noreply.github.com>
2021-04-28 13:45:47 +0200

    Update exec.bat

--- exec.bat
+++ exec.bat
@@ -15,7 +15,7 @@ PingCastle.exe --healthcheck --server %domain%
 PingCastle.exe --scanner smb --server %domain%
 PingCastle.exe --scanner share --server %domain%
 
-python parser.py %xml_location% %tags_location% %broker_ip% %domain% 
+python XMLparser.py %xml_location% %tags_location% %broker_ip% %domain% 
 python SMBParser.py %smb_report% %broker_ip% %domain% 
 python shareCounter.py %share_report% %broker_ip% %domain% 
 rem python userCounter.py %logs_file% %login_list% %broker_ip% %domain%
