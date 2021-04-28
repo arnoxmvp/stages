81bc3f2ff8806ab0690d49092d3a30c3b158556c
arnoxmvp <82814715+arnoxmvp@users.noreply.github.com>
2021-04-28 13:43:55 +0200

    Update XMLParser.py

--- XMLParser.py
+++ XMLParser.py
@@ -6,29 +6,32 @@ Takes 2 parameters. 1st is the xml report to parse and 2nd is the list of tags t
 Script written by Arnaud Collart the 15/04/2021.
 Documentations can be found on :  https://www.temporaryURL.com/doc/mydoc
 """
-
 # Import Librabries
 from xml.etree import ElementTree as eT
 from paho.mqtt import publish as pb
+import argparse
 
-#Create the variables 
+# Args define
+parser = argparse.ArgumentParser()
+parser.add_argument("xmlfile", help="File containing data")
+parser.add_argument("textags", help="File containing tags")
+parser.add_argument("hostip", help="IP of MQTT Broker")
+parser.add_argument("domain", help="Domain you'd like to work one")
+args = parser.parse_args()
+
+# Create the variables
 tagsDict = {}
-domain = "xxx"
-file = 'path_to_file'
-hostip = "mqtt_broker_ip"
-textags = "path_to_file"
 
-#Parse the XML File
-tree = eT.parse(file)
+# Parse the XML File
+tree = eT.parse(args.xmlfile)
 root = tree.getroot()
 
-#Search each tag given by the text file
-with open(textags) as tagsFile:
+# Search each tag given by the text file
+with open(args.textags) as tagsFile:
     for line in tagsFile:
         line = line.rstrip("\n")
         tagsDict[line] = root.find(line).text
-        print
-        (tagsDict[line])
+        print(tagsDict[line])
 
-#Send the tag values over MQTT
-        pb.single(topic, qos=0, auth=None, hostname=hostip)
+# Send the tag values over MQTT
+        pb.single("Security/ADDomain/" + args.domain + "/Pingcastle/" + line, tagsDict[line], hostname=args.hostip)
