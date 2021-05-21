769a2a26f0eda6f5bd80214075ef7bce1cc2cdb4
arnoxmvp <82814715+arnoxmvp@users.noreply.github.com>
2021-05-21 09:17:50 +0200

    Update 1.6

--- ADScript.ps1
+++ ADScript.ps1
@@ -1,5 +1,5 @@
 <#
-    ADScript v1.5
+    ADScript v1.6
    This script will retrieve some security data about the Windows AD domain of specified domain.
    Data retrieved contains :
             .Quantity of disabled, enabled, expired, unused, never used and locked users.
@@ -14,23 +14,23 @@
 
 
 #Establishing variables
-param($hostip, $server) 
+param($broker, $server, $cafile, $certfile, $keyfile) 
 $6months = 180
 $12months = 365
 $topics = @{
-    Disabled = "/Security/ADDomain/$server/IAM/Disabled"
-    Enabled = "/Security/ADDomain/$server/IAM/Enabled"
-    UnusedAccounts = "/Security/ADDomain/$server/IAM/UnusedAccount6Months"
-    NeverUsed = "/Security/ADDomain/$server/IAM/NeverUsed"
-    PwdUnchanged = "/Security/ADDomain/$server/IAM/PasswordUnchange1Year"
-    PwdLocked = "/Security/ADDomain/$server/IAM/Locked"
-    Expired = "/Security/ADDomain/$server/IAM/Expired"
-    PwdNeverChange = "/Security/ADDomain/$server/IAM/PwdNeverChange"
-    DomainAdmins = "/Security/ADDomain/$server/IAM/DomainAdmins"
-    Admins = "/Security/ADDomain/$server/IAM/Admins"
-    DisabledProtected = "/Security/ADDomain/$server/IAM/DisabledProtected"
-    Computers = "/Security/ADDomain/$server/IAM/Computers"
-    Gpos = "/Security/ADDomain/$server/IAM/UnlinkedGpos"
+    Disabled = "Security/ADDomain/$server/IAM/Disabled"
+    Enabled = "Security/ADDomain/$server/IAM/Enabled"
+    UnusedAccounts = "Security/ADDomain/$server/IAM/UnusedAccount6Months"
+    NeverUsed = "Security/ADDomain/$server/IAM/NeverUsed"
+    PwdUnchanged = "Security/ADDomain/$server/IAM/PasswordUnchanged1Year"
+    PwdLocked = "Security/ADDomain/$server/IAM/Locked"
+    Expired = "Security/ADDomain/$server/IAM/Expired"
+    PwdNeverChange = "Security/ADDomain/$server/IAM/PwdNeverChange"
+    DomainAdmins = "Security/ADDomain/$server/IAM/DomainAdmins"
+    Admins = "Security/ADDomain/$server/IAM/Admins"
+    DisabledProtected = "Security/ADDomain/$server/IAM/DisabledProtected"
+    Computers = "Security/ADDomain/$server/IAM/Computers"
+    Gpos = "Security/ADDomain/$server/IAM/UnlinkedGpos"
 }
 $since6months = [DateTime]::Today.AddDays($6months)
 $since12months = [DateTime]::Today.AddDays($12months)
@@ -60,17 +60,17 @@ $nbComputers = (Get-ADComputer -filter * -Server $server).count
 $nbUnlinkedGPO = ($GPOXmlReport.GPOS.GPO | Where-Object {$_.LinksTo -eq $null}).count 
 
 
-.\Mosquitto\mosquitto_pub.exe -t $topics.Enabled -m $nbEnabled -h $hostip
-.\Mosquitto\mosquitto_pub.exe -t $topics.Disabled -m $nbDisabled -h $hostip
-.\Mosquitto\mosquitto_pub.exe -t $topics.PwdLocked -m $nbLocked -h $hostip
-.\Mosquitto\mosquitto_pub.exe -t $topics.UnusedAccounts -m $nb6monthsNotUsed -h $hostip
-.\Mosquitto\mosquitto_pub.exe -t $topics.NeverUsed -m $nbNeverUsed -h $hostip
-.\Mosquitto\mosquitto_pub.exe -t $topics.Expired -m $nbExpired -h $hostip
-.\Mosquitto\mosquitto_pub.exe -t $topics.PwdUnchanged -m $nbYearUnchangedPwd -h $hostip
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.Enabled -m $nbEnabled -h $broker
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.Disabled -m $nbDisabled -h $broker
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.PwdLocked -m $nbLocked -h $broker
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.UnusedAccounts -m $nb6monthsNotUsed -h $broker
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.NeverUsed -m $nbNeverUsed -h $broker
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.Expired -m $nbExpired -h $broker
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.PwdUnchanged -m $nbYearUnchangedPwd -h $broker
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.PwdNeverChange -m $nbPassNeverChange -h $broker
 
-
-.\Mosquitto\mosquitto_pub.exe -t $topics.DomainAdmins -m $nbDomainAdmins -h $hostip
-.\Mosquitto\mosquitto_pub.exe -t $topics.Admins -m $nbAdmins -h $hostip
-.\Mosquitto\mosquitto_pub.exe -t $topics.DisabledProtected -m $nbDisabledProtected -h $hostip
-.\Mosquitto\mosquitto_pub.exe -t $topics.Computers -m $nbComputers -h $hostip
-.\Mosquitto\mosquitto_pub.exe -t $topics.Gpos -m $nbUnlinkedGPO -h $hostip
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.DomainAdmins -m $nbDomainAdmins -h $broker
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.Admins -m $nbAdmins -h $broker
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.DisabledProtected -m $nbDisabledProtected -h $broker
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.Computers -m $nbComputers -h $broker
+.\Mosquitto\mosquitto_pub.exe -p 8883 --cafile $cafile --cert $certfile --key $keyfile -t $topics.Gpos -m $nbUnlinkedGPO -h $broker
